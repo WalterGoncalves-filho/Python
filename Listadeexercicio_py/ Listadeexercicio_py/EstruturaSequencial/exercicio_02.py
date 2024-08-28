1d8e24c4f534e46491ff51676d9b32e4c3e397c0
Walter Gonçalves Baraúna Filho <163660669+WalterGoncalves-filho@users.noreply.github.com>
2024-08-28 15:52:31 -0400

    Delete Listadeexercicio_py/ Listadeexercicio_py/EstruturaSequencial directory

--- Listadeexercicio_py/ Listadeexercicio_py/EstruturaSequencial/exercicio_02.py
+++
@@ -1,8 +0,0 @@
-# Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
-
-nun = float(input(" Digite um número: "))
-
-# SÁIDA
-
-print("O número informado foi: ", nun)
-
--- Listadeexercicio_py/ Listadeexercicio_py/EstruturaSequencial/exercicio_07.py
+++
@@ -1,8 +0,0 @@
-# Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
-# LOGICA: 
-lados = float(input('digite o numero dos lados:'))
-area_quadrado = lados**2
-
-# SAIDA:
-
-print(area_quadrado,"m²")
