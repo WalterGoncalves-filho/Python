6b2c57bdd09169197ea5e6706073281939e36fba
Walter Gonçalves Baraúna Filho <163660669+WalterGoncalves-filho@users.noreply.github.com>
2024-09-11 19:53:55 -0400

    1° Commit

--- Funcoes/operacoes.py
+++ Funcoes/operacoes.py
@@ -9,7 +9,7 @@ def operacoes(a, b):
     else:
         print("error!")
     return 0
-num_1 = int(input())
-num_2 = int(input())
+num_1 = int(input("Digite um número inteiro: \n"))
+num_2 = int(input("Digite outro número inteiro: \n"))
 calculo = operacoes(num_1, num_2)
 print('{} + {} = {}\n{} - {} = {}\n{} * {} = {}\n{} / {} = {:.2f}'.format(num_1, num_2, calculo[0], num_1, num_2, calculo[1], num_1, num_2, calculo[2], num_1, num_2, calculo[3]))
