ff72c0bfd7d2147278fbb6078230b209289f4844
Walter Gonçalves Baraúna Filho <163660669+WalterGoncalves-filho@users.noreply.github.com>
2024-09-11 19:51:37 -0400

    1° Commit

---
+++ Funcoes/operacoes.py
@@ -0,0 +1,15 @@
+def operacoes(a, b):
+    soma = a + b
+    subtracao = a - b
+    multiplicacao = a * b
+    if b != 0:
+        divisao = a / b
+        result = [soma, subtracao, multiplicacao, divisao]
+        return result
+    else:
+        print("error!")
+    return 0
+num_1 = int(input())
+num_2 = int(input())
+calculo = operacoes(num_1, num_2)
+print('{} + {} = {}\n{} - {} = {}\n{} * {} = {}\n{} / {} = {:.2f}'.format(num_1, num_2, calculo[0], num_1, num_2, calculo[1], num_1, num_2, calculo[2], num_1, num_2, calculo[3]))
