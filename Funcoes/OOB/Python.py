d6b8373a1c35d18a5e8a781a75580da917778383
Walter Gonçalves Baraúna Filho <163660669+WalterGoncalves-filho@users.noreply.github.com>
2024-09-12 20:30:35 -0400

    1°Commit

---
+++ Funcoes/OOB/Python.py
@@ -0,0 +1,42 @@
+def operacoes(a, b):
+    if b != 0:
+        divisao = a / b
+        soma = a + b
+        subtracao = a - b
+        multiplicacao = a * b
+        result = [soma, subtracao, multiplicacao, divisao]
+        return result
+    else:
+        print("Error!")   
+
+num_1 = int(input("Digite um número inteiro: \n"))
+num_2 = int(input("Digite outro número inteiro: \n"))
+calculo = operacoes(num_1, num_2)
+print(f'{num_1} + {num_2} = {calculo[0]}\n{num_1} - {num_2} = {calculo[1]}\n{num_1} * {num_2} = {calculo[2]}\n{num_1} / {num_2} = {calculo[3]:.4f}')
+
+
+'''
+def soma(a, b):
+    soma = a + b
+    return soma
+def subtracao(a, b):
+    subtracao = a - b
+    return subtracao
+def multiplicacao(a, b):
+    multiplicacao = a * b
+    return multiplicacao
+def divisao(a, b):
+    if b != 0:
+        divisao = a / b
+        return divisao
+    return"Error"
+
+a = int(input("Digite um numero inteiro: "))
+b = int(input("Digite um numero inteiro: "))
+print(f'{a} + {b} = {soma(a, b)}\n{a} - {b} = {subtracao(a, b)}\n{a} * {b} = {multiplicacao(a, b)}\n{a} / {b} = {divisao(a, b):.2f}')
+'''
+
+
+
+
+
---
+++ Funcoes/OOB/pessoa.java
@@ -0,0 +1,6 @@
+class pessoa {
+
+
+
+};
+
