feb5808771e6d4b1f2ddc51062405a7e26cdb3d3
Walter Gonçalves Baraúna Filho <163660669+WalterGoncalves-filho@users.noreply.github.com>
2024-09-18 23:57:51 -0400

    Update operacoes.py

--- Funcoes/operacoes.py
+++ Funcoes/operacoes.py
@@ -15,21 +15,6 @@ num_2 = int(input("Digite outro número inteiro: \n"))
 calculo = operacoes(num_1, num_2)
 print('{} + {} = {}\n{} - {} = {}\n{} * {} = {}\n{} / {} = {:.2f}'.format(num_1, num_2, calculo[0], num_1, num_2, calculo[1], num_1, num_2, calculo[2], num_1, num_2, calculo[3]))
 '''
-def operacoes(a, b):
-    if b != 0:
-        divisao = a / b
-        soma = a + b
-        subtracao = a - b
-        multiplicacao = a * b
-        result = [soma, subtracao, multiplicacao, divisao]
-        return result
-    else:
-        print("Error!")   
-
-num_1 = int(input("Digite um número inteiro: \n"))
-num_2 = int(input("Digite outro número inteiro: \n"))
-calculo = operacoes(num_1, num_2)
-print(f'{num_1} + {num_2} = {calculo[0]}\n{num_1} - {num_2} = {calculo[1]}\n{num_1} * {num_2} = {calculo[2]}\n{num_1} / {num_2} = {calculo[3]:.2f}')
 
 '''
 def soma(a, b):
@@ -53,6 +38,17 @@ print(f'{a} + {b} = {soma(a, b)}\n{a} - {b} = {subtracao(a, b)}\n{a} * {b} = {mu
 '''
 
 
+def operacoes(a, b):
+    if b != 0:
+        divisao = a / b
+        soma = a + b
+        subtracao = a - b
+        multiplicacao = a * b
+        print(f'{num_1} + {num_2} = {soma}\n{num_1} - {num_2} = {subtracao}\n{num_1} * {num_2} = {multiplicacao}\n{num_1} / {num_2} = {divisao:.2f}')
+    else:
+        print("Error!Nao eh possivel dividir por zero!")   
+num_1, num_2  = map(int, input("Digite dois números inteiros separados por espaços: ").split())
+calculo = operacoes(num_1, num_2)
 
 
 
