f6689cdc2acb69ec6c1c313a4dc693da6883acf8
Walter Gonçalves Baraúna Filho <163660669+WalterGoncalves-filho@users.noreply.github.com>
2024-11-07 19:26:28 -0400

    Update Jogo do ano.py

--- projects/Jogo do ano.py
+++ projects/Jogo do ano.py
@@ -1,3 +1,4 @@
+
 #pergunta = "Em que ano o Brasil conquistou o pentacampionato no futebol?"
 
 palpites1 = []
@@ -14,21 +15,28 @@ dica =  'coisa de ver'
 pular = "\n"
 print(100*pular)
 
-palpite1 = ""
+print(f"Pergunta do jogador 1:\n{pt1}")
+print(f'Dica da pergunta: {dica}')
+print("\n\n")
 
-
-print(pt1)
-print(dica)
+palpite1 = str(input("Informe seu palpite: "))
+print("")
 
 
 while palpite1 != gabarito1:
-    palpite1 = str(input("Informe seu palpite: "))
-    palpites1.append(palpite1)
-    print("resposta invalida.tente novamente!")
+    if palpite1 != gabarito1:
+        print("resposta invalida.tente novamente!")
+        palpites1.append(palpite1)
+        palpite1 = str(input("Informe seu palpite: "))
+        print("")
+    else:
+        break
 
-
-if gabarito1 == palpite1:
+if palpite1 == gabarito1:
     print(f"Parabéns {jogador_1} voçê acertou!")
+    palpites1.append(palpite1)
+
+
 
 
 #==================================================================
@@ -39,6 +47,7 @@ print(2*"\n")
 # pt2 = str(input("Informe a pergunta a ser feita: "))
 # gabarito2 =  str(input("Informe seu gabarito: "))
 # dica = str(input("Informe sua dica: "))
+
 jogador_2 = 'WALTER'
 pt2 = 'v e f '
 gabarito2 =  '10'
@@ -47,34 +56,41 @@ dica = 'carecas'
 pular = "\n"
 print(100*pular)
 
-print(pt2)
-print(dica)
+print(f"Pergunta do jogador 2:\n{pt2}")
+print(f'Dica da pergunta: {dica}')
 
-palpite2 = ""
+palpite2 = str(input("Informe seu palpite: \n"))
+print("")
+
 
 while palpite2 != gabarito2:
-    palpite2 = str(input("Informe seu palpite: "))
-    palpites2.append(palpite2)
-    print("resposta invalida.tente novamente!")
+    if palpite2 != gabarito2:
+        print("resposta invalida.tente novamente!")
+        palpites2.append(palpite2)
+        palpite2 = str(input("Informe seu palpite: \n"))
+        print("")
+    else:
+        break
 
-if gabarito2 == palpite2:
+if palpite2 == gabarito2:
     print(f"Parabéns {jogador_2} voçê acertou!")
+    palpites2.append(palpite2)
 
 
-print("\n")
+print("")
 
-print(f"A quantidade de palpites do {jogador_1} é de {len(palpites1)}")
+print(f"A quantidade de palpites do {jogador_1} : {len(palpites1)}")
 print(f"Palpites do {jogador_1}:\n{palpites1}")
 print("\n")
 
-print(f"A quantidade de palpites do {jogador_2} é de {len(palpites2)}")
-print(f"Palpites do {jogador_2}\n{palpites2}")
+print(f"A quantidade de palpites do {jogador_2}: {len(palpites2)}")
+print(f"Palpites do {jogador_2}:\n{palpites2}")
 print("\n")
 
 if len(palpites1) > len(palpites2):
-    print(f"O jogador {jogador_1} Venceu!")
+    print(f"{jogador_1} Venceu!")
 elif len(palpites1) < len(palpites2):
-    print(f"O jogador {jogador_2} venceu!")
+    print(f"{jogador_2} venceu!")
 else:
     print("EMPATE!!\n")
 
